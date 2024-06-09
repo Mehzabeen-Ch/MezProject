86ab640c69b33b80cfd966a7b94f0088708a0749
Mehzabeen-Ch <mehzabeenmaniar@gmail.com>
2024-06-09 04:35:38 +0100

    add style in home page to link to myPage2

--- content/.eleventy.js
+++ content/.eleventy.js
@@ -1,5 +1,16 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img")
     eleventyConfig.addPassthroughCopy("css/fonts")
-    eleventyConfig.addPassthroughCopy("/content/bundle.css");
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addWatchTarget("./src/css/");
+
+    return{
+
+    dir:{
+
+    input: "src",
+    output: "public",
+    },
 };
+};
+   
